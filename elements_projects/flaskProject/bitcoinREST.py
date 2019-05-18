9e0e43bc74c723712ab4d418ce06b40bb7e45470
Roberto Serrano <rserranon@gmail.com>
2019-05-17 21:00:41 -0500

    Working with RPC functions and also Flask

--- elements_projects/flaskProject/bitcoinREST.py
+++ elements_projects/flaskProject/bitcoinREST.py
@@ -1,14 +1,16 @@
-
 from flask import Flask
+from decimal import *
+from flask import json, jsonify
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from flask_cors import CORS
 
-rpc_port = 19001
+rpc_port = 18443
 rpc_user = 'admin1'
 rpc_password = '123'
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 def CallRPC( commands ):
     global rpc_port
@@ -18,34 +20,30 @@ def CallRPC( commands ):
     try:
         rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:%s"%(rpc_user, rpc_password, rpc_port))
         print (f"connection: {rpc_connection}")
-
-        jsonResult = rpc_connection.batch_(commands)
+        result = rpc_connection.batch_(commands)
+        print(f"Result type: {type(result)}")
     except JSONRPCException as json_exception:
         return "A JSON RPC Exception occured: " + str(json_exception)
     except Exception as general_exception:
         return "An Exception occured: " + str(general_exception)
-    return jsonResult
+    return result
 
 
 @app.route("/")
 def home():
-    result = CallRPC([["getwalletinfo", ""]])
-    return str(result['balance'])
+    return jsonify(CallRPC([["getblockchaininfo", '["chain":"regtest"]']]))
 
 @app.route("/getwalletinfo")
 def getwalletinfo():
-    result = CallRPC([["getwalletinfo", None]])
-    return str(result)
+    return jsonify(CallRPC([["getwalletinfo", "[]"]]))
 
 @app.route("/listunspent")
 def listunspent():
-    result = CallRPC([["listunspent", None]])
-    return str(result)
+    return jsonify(CallRPC([["listunspent", None]]))
 
 @app.route("/listtransactions")
 def llisttransactions():
-    result = CallRPC([["listtransactions", None]])
-    return str(result)
+    return jsonify(CallRPC([["listtransactions", None]]))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6002)
---
+++ elements_projects/flaskProject/bitcoinWalletREST.py
@@ -0,0 +1,77 @@
+# Adapted from @Gavin Andresen and Taylor Gering spendfrom
+
+from decimal import *
+import time
+import logging
+import json
+from flask import json, jsonify
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+logging.basicConfig(level=logging.DEBUG)
+
+def check_json_precision():
+    pass
+   #"""Make sure json library being used does not lose precision converting BTC values"""
+
+class BitcoinWalletRPC:
+    def __init__(self, connectURI, passphrase = "", fee = Decimal("0.0001"), unlockseconds =5, minconfs = 1):
+    #call to verify JSON precision, TODO implement
+    #check_json_precision()
+        self.logger = logging.getLogger(__name__)
+        self.BASE_FEE = fee
+        self.MINCONFS = minconfs
+        self.UNLOCKSECONDS = unlockseconds
+
+        # get connection to bitcoind daemon
+        self.bitcoindConnection = self.connect_JSON(connectURI, passphrase)
+
+    def connect_JSON(self, uri, passphrase):
+        """Connect to a bitcoin JSON-RPC server"""
+        try:
+            result = AuthServiceProxy(uri)
+            # ServiceProxy is lazy-connect, so send an RPC command mostly to catch connection errors,
+            # but also make sure the bitcoind we're talking to is/isn't testnet:
+            self.logger.info(f"Connected to {result.getmininginfo()['chain']}")
+            print ("result")
+            print (result)
+            result.passphrase = passphrase
+
+            return result
+        except:
+            raise
+
+    def list_addresses(self, searchaccount=None):
+        self.logger.debug(f"list addresses called with search account = {searchaccount}")
+        address_to_account = dict()
+        for info in self.bitcoindConnection.listreceivedbyaddress(self.MINCONFS):
+            self.logger.debug(f"info {info}")
+            if not searchaccount or searchaccount == info["account"]:
+                address_to_account[info["address"]] = info["account"]
+
+        self.logger.debug("list_addresses returns object of length %d", len(address_to_account))
+        self.logger.debug("list_addresses returns object of length %s", address_to_account)
+        return address_to_account
+
+    def walletInfo(self, targetwallet=""):
+        self.logger.debug(f"get wallet {targetwallet} information")
+        return self.bitcoindConnection.getwalletinfo(targetwallet)
+
+    def createwallet(self, walletname):
+        self.logger.debug(f"create wallet with name: {walletname}")
+        return self.bitcoindConnection.createwallet(walletname)
+
+    def listtransactions(self):
+        transactions = self.bitcoindConnection.listtransactions()
+        self.logger.debug(transactions)
+        return transactions
+
+RPCUSER = "admin1"
+RPCPASS = "123"
+RPCPORT = 18443
+myWalletConnection = BitcoinWalletRPC("http://%s:%s@127.0.0.1:%s"%(RPCUSER, RPCPASS, RPCPORT))
+
+myWalletConnection.list_addresses()
+#myWalletConnection.createwallet("testwallet")
+#myWalletConnection.walletInfo("")
+myWalletConnection.listtransactions()
+
