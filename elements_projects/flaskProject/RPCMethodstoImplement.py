73092bc74dcf7fd5b9a9e52aad00c9e91020e8dc
Roberto Serrano <rserranon@gmail.com>
2019-05-18 20:30:16 -0500

    Refactor bitcoinRPC.py and create a list of methods to implement based on bitcoincore.org RPC documentation

---
+++ elements_projects/flaskProject/RPCMethodstoImplement.py
@@ -0,0 +1,116 @@
+# RPC methods to implement
+# Original list obtained from MetacoSA/NBitcoin licensed under the MIT License
+# acording to: bitcoincore v0.18.0
+# Link: https://bitcoincore.org/en/doc/0.18.0/rpc/network/getnetworkinfo/i
+#
+# TODO final reorder according to bitcoincore.org documentation and link, to be done while implementing each method
+#
+# Category		    	Name			            Implemented
+#------------------     ---------------------------         -----------------------
+# ------------------ BLOCKCHAIN and UTXO
+# blockchain		        getblockchaininfo                       Yes
+# blockchain		        getblockcount
+# blockchain		        getblock
+# blockchain		        getblockhash
+# blockchain		        getchaintips
+# blockchain		        getdifficulty
+# blockchain		        getmempoolinfo
+# blockchain		        getrawmempool
+# blockchain		        gettxout
+# blockchain		        gettxoutproof
+# blockchain		        verifytxoutproof
+# blockchain		        gettxoutsetinfo
+# blockchain		        verifychain
+#------------------- CONTROL
+# callscontrol			getinfo
+# callscontrol			help
+# callscontrol			stop
+# ------------------ GENERATING
+# generating		        generate
+# generating		        generatetoaddess
+# ------------------ MINING
+# mining			 getblocktemplate
+# mining			 getmininginfo
+# mining			 getnetworkhashps
+# mining			 prioritisetransaction
+# mining			 submitblock
+# ------------------ NETWORK
+# network			getnetworkinfo
+# network			addnode
+# network			disconnectnode
+# network			getaddednodeinfo
+# network			getconnectioncount
+# network			getnettotals
+# network			getpeerinfo
+# network			ping
+# network			setban
+# network			listbanned
+# network			clearbanned
+# ------------------ RAW TRANSACTIONS
+# rawtransactions	        createrawtransaction
+# rawtransactions	        decoderawtransaction
+# rawtransactions	        decodescript
+# rawtransactions	        getrawtransaction
+# rawtransactions	        sendrawtransaction
+# rawtransactions	        signrawtransaction
+# rawtransactions	        fundrawtransaction
+# ------------------ UTIL
+# util			        createmultisig
+# util			        validateaddress
+# util			        verifymessage
+# util			        estimatefee
+# util			        estimatesmartfee
+# ------------------ WALLET
+# wallet			 addmultisigaddress
+# wallet			 backupwallet
+# wallet                         createwallet                           Yes
+# wallet			 dumpprivkey
+# wallet			 dumpwallet
+# wallet			 encryptwallet
+# wallet			 getaccountaddress
+# wallet			 getaccount
+# wallet			 getaddressesbyaccount
+# wallet			 getbalance
+# wallet			 getnewaddress
+# wallet			 getrawchangeaddress
+# wallet			 getreceivedbyaccount
+# wallet			 getreceivedbyaddress
+# wallet			 gettransaction
+# wallet			 getunconfirmedbalance
+# wallet			 getwalletinfo                          Yes
+# wallet			 importprivkey
+# wallet			 importwallet
+# wallet			 importaddress
+# wallet			 keypoolrefill
+# wallet			 listaccounts
+# wallet			 listaddressgroupings
+# wallet			 listlockunspent
+# wallet			 listreceivedbyaccount
+# wallet			 listreceivedbyaddress                  Yes
+# wallet			 listsinceblock
+# wallet			 listtransactions                       Yes
+# wallet			 listunspent
+# wallet			 lockunspent
+# wallet			 move
+# wallet			 sendfrom
+# wallet			 sendmany
+# wallet			 sendtoaddress
+# wallet			 setaccount
+# wallet			 settxfee
+# wallet			 signmessage
+# wallet			 walletlock
+# wallet			 walletpassphrasechange
+# wallet			 walletpassphrase
+# wallet			 walletprocesspsbt
+# ------------------ PSBT
+# psbt - decodepsbt
+# psbt - combinepsbt
+# psbt - finalizepsbt
+# psbt - createpsbt
+# psbt - convertopsbt
+# wallet			 walletcreatefundedpsbt
+# ------------------ t shown in help
+# hidden			invalidateblock
+# hidden			reconsiderblock
+# hidden			setmocktime
+# hidden			resendwallettransactions
--- elements_projects/flaskProject/bitcoinRPC.py
+++ elements_projects/flaskProject/bitcoinRPC.py
@@ -10,22 +10,27 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 logging.basicConfig(level=logging.DEBUG)
 
 def check_json_precision():
-    pass
-   #"""Make sure json library being used does not lose precision converting BTC values"""
+    """Make sure json library being used does not lose precision converting BTC values"""
+    n = Decimal("20000000.00000003")
+    satoshis = int(json.loads(json.dumps(float(n)))*1.0e8)
+    if satoshis != 2000000000000003:
+        raise RuntimeError("JSON encode/decode loses precision")
 
-class BitcoinWalletRPC:
+class RPCbitcoin:
     def __init__(self, connectURI, passphrase = "", fee = Decimal("0.0001"), unlockseconds =5, minconfs = 1):
-    #call to verify JSON precision, TODO implement
-    #check_json_precision()
+
+        #call to verify JSON precision
+        check_json_precision()
+
         self.logger = logging.getLogger(__name__)
         self.BASE_FEE = fee
         self.MINCONFS = minconfs
         self.UNLOCKSECONDS = unlockseconds
 
-        # get connection to bitcoind daemon
-        self.bitcoindConnection = self.connect_JSON(connectURI, passphrase)
+        # get RPC connection to bitcoind daemon
+        self.bitcoindConnection = self.connectRPC(connectURI, passphrase)
 
-    def connect_JSON(self, uri, passphrase):
+    def connectRPC(self, uri, passphrase):
         """Connect to a bitcoin JSON-RPC server"""
         try:
             result = AuthServiceProxy(uri)
@@ -40,40 +45,42 @@ class BitcoinWalletRPC:
         except:
             raise
 
-    def list_addresses(self, searchaccount=None):
-        self.logger.debug(f"list addresses called with search account = {searchaccount}")
+    def listrecievedbyaddress(self, searchAddress=None):
+        self.logger.debug(f"list addresses called with search account = {searchAddress}")
         address_to_account = dict()
-        for info in self.bitcoindConnection.listreceivedbyaddress(self.MINCONFS):
+        # TODO verify that searchAddress can be passed as None
+        for info in self.bitcoindConnection.listreceivedbyaddress(self.MINCONFS, searchAddress):
             self.logger.debug(f"info {info}")
-            if not searchaccount or searchaccount == info["account"]:
+            # TODO, verify if account is a valid key and the logic of this if
+            if not searchAddress or searchAdress == info["account"]:
                 address_to_account[info["address"]] = info["account"]
 
-        self.logger.debug("list_addresses returns object of length %d", len(address_to_account))
-        self.logger.debug("list_addresses returns object of length %s", address_to_account)
+        self.logger.debug("list_addresses returns object of length {len(address_to_account)}")
         return address_to_account
 
-    def walletInfo(self, targetwallet=""):
+    def getwalletinfo(self, targetwallet=""):
         self.logger.debug(f"get wallet {targetwallet} information")
-        # it doesn't work incliding the parameter self.bitcoindConnection.getwalletinfo(targetwallet) so I use self.bitcoindConnection.getwalletinfo()
+        # it doesn't work using the parameter targetwallet self.bitcoindConnection.getwalletinfo(targetwallet)
+        # so I used self.bitcoindConnection.getwalletinfo(), TODO find a fix for thix to use several wallets
         return self.bitcoindConnection.getwalletinfo()
 
+    # Createwallet it does work but we need to understand better how to switch walltets
+    # and make getwalletinfo work with the parameter targetwallet
     def createwallet(self, walletname):
         self.logger.debug(f"create wallet with name: {walletname}")
         return self.bitcoindConnection.createwallet(walletname)
 
     def listtransactions(self):
         transactions = self.bitcoindConnection.listtransactions()
-        self.logger.debug(transactions)
+        self.logger.debug(f"Number of transactions = {len(transactions)}")
         return transactions
 
 RPCUSER = "admin1"
 RPCPASS = "1234"
 RPCPORT = 18443
-myWalletConnection = BitcoinWalletRPC("http://%s:%s@127.0.0.1:%s"%(RPCUSER, RPCPASS, RPCPORT))
+myWalletConnection = RPCbitcoin("http://%s:%s@127.0.0.1:%s"%(RPCUSER, RPCPASS, RPCPORT))
 
-myWalletConnection.list_addresses()
 #myWalletConnection.createwallet("testwallet")
-# TODO RPC method getwalletinfo is not working
-myWalletConnection.walletInfo("")
+myWalletConnection.getwalletinfo()
 myWalletConnection.listtransactions()
-
+myWalletConnection.listrecievedbyaddress()
